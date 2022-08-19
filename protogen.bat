ae8d00e26efd9d07a2dd3e029ef1514afbbcbd40
indiest <indiestian@gmail.com>
2022-08-19 18:58:02 +0800

    - Switched the version of Protobuf to 3.21.5 - Added ChanneldReplicationDriver - Added ChanneldSceneComponent

---
+++ protogen.bat
@@ -0,0 +1,30 @@
+@echo off
+SET PROJECT_ROOT=%cd%
+SET PROTOBUF_PATH=%PROJECT_ROOT%/Plugins/ue4-protobuf/Source/Protobuf/ThirdParty
+SET PROTOC="%PROTOBUF_PATH%/Bin/protoc.exe"
+
+cd "%PROJECT_ROOT%/Source/ChanneldIntegration"
+
+%PROTOC% --cpp_out=. -I "%CHANNELD_PATH%/examples/channeld-ue-chat/chatpb" chat.proto
+del chat.pb.cpp
+rename chat.pb.cc chat.pb.cpp
+
+%PROTOC% --cpp_out=. -I "%CHANNELD_PATH%/examples/channeld-ue-tps/tpspb" -I "%CHANNELD_PATH%" tps.proto
+del tps.pb.cpp
+rename tps.pb.cc tps.pb.cpp
+
+cd "%PROJECT_ROOT%/Plugins/ChanneldUE/Source/ChanneldUE"
+
+%PROTOC% --cpp_out=. -I "%CHANNELD_PATH%/pkg/channeldpb" -I "%PROTOBUF_PATH%" channeld.proto
+del channeld.pb.cpp
+rename channeld.pb.cc channeld.pb.cpp
+
+%PROTOC% --cpp_out=. -I "%CHANNELD_PATH%/internal/testpb" -I "%PROTOBUF_PATH%" test.proto
+del test.pb.cpp
+rename test.pb.cc test.pb.cpp
+
+%PROTOC% --cpp_out=. -I "%CHANNELD_PATH%/pkg/channeldpb" unreal_common.proto
+del unreal_common.pb.cpp
+rename unreal_common.pb.cc unreal_common.pb.cpp
+
+pause
