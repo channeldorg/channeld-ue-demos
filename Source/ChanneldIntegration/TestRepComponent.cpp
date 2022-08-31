b256fd0f3432024af435856deb0515bd5ad8854d
indiest <indiestian@gmail.com>
2022-08-31 18:12:26 +0800

    - Implemented the Character replicator (except the RootMotion part)

--- Source/ChanneldIntegration/TestRepComponent.cpp
+++ Source/ChanneldIntegration/TestRepComponent.cpp
@@ -4,18 +4,32 @@
 #include "TestRepComponent.h"
 #include "tps.pb.h"
 #include "unreal_common.pb.h"
+#include "GameFramework/Character.h"
 
 const google::protobuf::Message* UTestRepComponent::GetStateFromChannelData(google::protobuf::Message* ChannelData, UObject* TargetObject, uint32 NetGUID, bool& bIsRemoved)
 {
 	auto TestRepChannelData = static_cast<tpspb::TestRepChannelData*>(ChannelData);
-	auto States = TestRepChannelData->mutable_scenecomponentstates();
-	if (States->contains(NetGUID))
+	if (TargetObject->IsA<USceneComponent>())
 	{
-		auto State = &States->at(NetGUID);
-		bIsRemoved = State->removed();
-		return State;
+		auto States = TestRepChannelData->mutable_scenecomponentstates();
+		if (States->contains(NetGUID))
+		{
+			auto State = &States->at(NetGUID);
+			bIsRemoved = State->removed();
+			return State;
+		}
 	}
-
+	else if (TargetObject->IsA<ACharacter>())
+	{
+		auto States = TestRepChannelData->mutable_characterstates();
+		if (States->contains(NetGUID))
+		{
+			auto State = &States->at(NetGUID);
+			bIsRemoved = State->removed();
+			return State;
+		}
+	}
+	
 	bIsRemoved = false;
 	return nullptr;
 }
@@ -28,6 +42,12 @@ void UTestRepComponent::SetStateToChannelData(const google::protobuf::Message* S
 		auto SceneCompState = static_cast<const unrealpb::SceneComponentState*>(State);
 		auto States = TestRepChannelData->mutable_scenecomponentstates();
 		(*States)[NetGUID] = *SceneCompState;
+	}
+	else if (TargetObject->IsA<ACharacter>())
+	{
+		auto CharacterState = static_cast<const unrealpb::CharacterState*>(State);
+		auto States = TestRepChannelData->mutable_characterstates();
+		(*States)[NetGUID] = *CharacterState;
 	}
 }
 
