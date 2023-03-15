3e29cface2604e7da10a13b5936b0dc8cfe63ed0
indiest <indiestian@gmail.com>
2023-03-15 19:04:35 +0800

    - Removed copyright header in all code files

--- Source/ChanneldIntegration/Tests/FlatStatesReflectionBenchmark.cpp
+++ Source/ChanneldIntegration/Tests/FlatStatesReflectionBenchmark.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "CoreTypes.h"
 #include "FFlatStatesChannelDataProcessor.h"
 #include "ChanneldIntegration/tps.pb.h"
