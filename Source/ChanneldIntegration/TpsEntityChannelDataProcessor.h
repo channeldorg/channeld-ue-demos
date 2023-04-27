81b542bd6b97434416af31e08322566aae28068c
indiest <indiestian@gmail.com>
2023-04-27 10:49:32 +0800

    - Removed IChannelDataProcessor::GetRelevantNetGUIDsFromChannelData; - CheckUnspawnedObject now can resolve the Actor directly from the SpatialChannelData;

--- Source/ChanneldIntegration/TpsEntityChannelDataProcessor.h
+++ Source/ChanneldIntegration/TpsEntityChannelDataProcessor.h
@@ -10,6 +10,4 @@ public:
 	
 	virtual const google::protobuf::Message* GetStateFromChannelData(google::protobuf::Message* ChannelData, UClass* TargetClass, uint32 NetGUID, bool& bIsRemoved) override;
 	virtual void SetStateToChannelData(const google::protobuf::Message* State, google::protobuf::Message* ChannelData, UClass* TargetClass, uint32 NetGUID) override;
-
-	virtual TArray<uint32> GetRelevantNetGUIDsFromChannelData(const google::protobuf::Message* ChannelData) override;
 };
--- Source/ChanneldIntegration/TpsSpatialChannelDataProcessor.cpp
+++ Source/ChanneldIntegration/TpsSpatialChannelDataProcessor.cpp
@@ -56,36 +56,3 @@ void FTpsSpatialChannelDataProcessor::SetStateToChannelData(const google::protob
 	}
 }
 
-TArray<uint32> FTpsSpatialChannelDataProcessor::GetRelevantNetGUIDsFromChannelData(const google::protobuf::Message* ChannelData)
-{
-	auto SpatialChannelData = static_cast<const unrealpb::SpatialChannelData*>(ChannelData);
-	TArray<uint32> Result;
-	for (auto& Pair : SpatialChannelData->entities())
-	{
-		bool bIgnore = false;
-		// for (int i = 0; i < Pair.second.objref().context_size(); i++)
-		{
-			TCHAR* ClassPath = UTF8_TO_TCHAR(Pair.second.objref().classpath().c_str());//Pair.second.objref().context(0).pathname().c_str());
-			UClass* EntityClass = LoadObject<UClass>(nullptr, ClassPath);
-			
-			if (EntityClass == nullptr)
-			{
-				continue;
-			}
-			
-			if (EntityClass->IsChildOf(APlayerController::StaticClass()) ||
-				EntityClass->IsChildOf(APlayerState::StaticClass()))
-			{
-				bIgnore = true;
-				break;;
-			}
-		}
-
-		if (!bIgnore)
-		{
-			Result.Add(Pair.first);
-		}
-	}
-
-	return Result;
-}
