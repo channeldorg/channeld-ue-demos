a8de3ef242516030479957da815dabd60a51e182
indiest <indiestian@gmail.com>
2023-08-25 14:27:08 +0800

    - Added test cases for array codegen and replication; - Removed registeration of the customized FTestRepPlayerControllerReplicator;

--- Source/ChanneldIntegration/TestReplicatorsRegistration.h
+++ Source/ChanneldIntegration/TestReplicatorsRegistration.h
@@ -14,7 +14,7 @@ public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override
 	{
 		REGISTER_REPLICATOR_BP_SINGLETON(FTestRepGameStateReplicator, "/Game/Blueprints/BP_RepGameState.BP_RepGameState_C");
-		REGISTER_REPLICATOR_BP(FTestRepPlayerControllerReplicator, "/Game/Blueprints/BP_TestRepPlayerController.BP_TestRepPlayerController_C");
+		// REGISTER_REPLICATOR_BP(FTestRepPlayerControllerReplicator, "/Game/Blueprints/BP_TestRepPlayerController.BP_TestRepPlayerController_C");
 		REGISTER_REPLICATOR_BP(FTestNPCReplicator, "/Game/AI/BP_TestNPC.BP_TestNPC_C");
 	}
 };
